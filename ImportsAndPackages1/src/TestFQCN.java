082a85e24c588887af463cf40df20e153c61f04d
Mostafizur0 <mrahmansabbir@gmail.com>
2021-07-24 15:27:37 +0600

    imports & packages

---
+++ ImportsAndPackages1/src/TestFQCN.java
@@ -0,0 +1,9 @@
+public class TestFQCN {
+    public static void main(String[] args) {
+
+        // Using a FQCN when using the String class in the java.lang package
+        java.lang.String s = "We are testing ways to import classes";
+        System.out.println(s);
+        garden.vagitable.VineVegetable.main(args);
+    }
+}
---
+++ ImportsAndPackages1/src/TestImport.java
@@ -0,0 +1,30 @@
+// single-type-import declaration
+import garden.vagitable.VineVegetable;
+// type-import-on-demand declaration
+import garden.*; // will import vagitable package not classes inside it
+import garden.vagitable.*;
+import nursery.vegetable.*;
+// creates import conflict. will work if single-type-import is uncommented
+// more specific import will take precedence in case of conflicted import
+
+public class TestImport {
+    public static void main(String[] args) {
+
+        // String class in the java.lang package, simple class name ok
+        // no import statement required
+        String s = "We are testing import statements";
+        System.out.println(s);
+
+        // We use simple name because we specified location in the import
+        // statement
+        VineVegetable.main(args);
+        nursery.vegetable.VineVegetable.main(args);
+    }
+}
+
+// cannot create a class with same name as single-type-import class
+class VineVegetable {
+    public static void main(String[] args) {
+        System.out.println("I am a local VineVegetable");
+    }
+}
